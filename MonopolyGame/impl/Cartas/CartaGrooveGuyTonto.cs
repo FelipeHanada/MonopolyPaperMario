df55878ef1de55306cd8a5fe712a4018656eadce
Cauã Bassin <cauabassin@id.uff.br>
2025-10-22 19:09:01 -0300

    primeira carta implementada

---
+++ MonopolyGame/impl/Cartas/CartaGrooveGuyTonto.cs
@@ -0,0 +1,18 @@
+﻿using MonopolyPaperMario.MonopolyGame.Interface;
+using MonopolyPaperMario.MonopolyGame.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonopolyGame.impl.Cartas
+{
+    internal class CartaGrooveGuyTonto : CartaSorte
+    {
+        public CartaGrooveGuyTonto() : base("Groove Guy te deixou tonto. Você vai se mover na direção contrária no próximo turno.", new ReverterDirecaoJogador())
+        {
+           
+        }
+    }
+}
---
+++ MonopolyGame/impl/ReverterDirecaoJogador.cs
@@ -0,0 +1,18 @@
+﻿using MonopolyPaperMario.MonopolyGame.Interface;
+using MonopolyPaperMario.MonopolyGame.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonopolyGame.impl
+{
+    internal class ReverterDirecaoJogador : IEfeitoJogador
+    {
+        public void Execute(Jogador jogador)
+        {
+            jogador.Reverso = true;
+        }
+    }
+}
