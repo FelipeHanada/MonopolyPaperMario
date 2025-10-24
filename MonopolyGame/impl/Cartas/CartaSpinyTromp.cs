da2e8d0b6e79f4cfbebd69c710d68abb207c1f2e
GabrielCota121 <gabrielotavio@id.uff.br>
2025-10-24 07:32:52 -0300

    Efeito Spiny Tromp

---
+++ MonopolyGame/impl/Cartas/CartaSpinyTromp.cs
@@ -0,0 +1,18 @@
+using MonopolyPaperMario.MonopolyGame.Interface;
+using MonopolyPaperMario.MonopolyGame.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonopolyPaperMario.MonopolyGame.Impl.Cartas
+{
+    internal class CartaSpinyTromp : CartaSorte
+    {
+        public CartaSpinyTromp(Tabuleiro tabuleiro) : base("Oh não, um Spiny Tromp. Fuja dele e avance 4 casas", new EfeitoMoverJogador(tabuleiro,4))
+        {
+           
+        }
+    }
+}
