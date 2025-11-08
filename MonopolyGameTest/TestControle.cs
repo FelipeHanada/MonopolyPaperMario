2a1e82808a014c241ae1a840793afcfad7e46ca4
FelipeHanada <146875733+FelipeHanada@users.noreply.github.com>
2025-11-08 17:10:00 -0300

    add player actions troca

---
+++ MonopolyGameTest/TestControle.cs
@@ -0,0 +1,25 @@
+﻿using MonopolyGame.Interface.Controles;
+using MonopolyGame.Impl.Controles;
+using MonopolyGame.Model.Leiloes;
+using MonopolyGame.Model.Partidas;
+using MonopolyGame.Model.PossesJogador;
+using System.Runtime.ConstrainedExecution;
+using MonopolyGame.Interface.Partidas;
+
+namespace MonopolyGameTest
+{
+    [TestClass]
+    public sealed class TestControle
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Partida partida = new(["J1", "J2", "J3", "J4"]);
+            IControlePartida controle = new ControlePartida(partida);
+
+            Assert.AreEqual(partida.Jogadores[0], controle.Partida.JogadorAtual);
+            controle.Comum_RolarDados();
+            Assert.AreEqual(partida.Jogadores[0], controle.Partida.JogadorAtual);
+        }
+    }
+}
---
+++ MonopolyPaperMario/Components/Stores/ControlePartidaStore.cs
@@ -0,0 +1,29 @@
+﻿using MonopolyGame.Interface.Controles;
+using MonopolyGame.Impl.Controles;
+using MonopolyGame.Model.Partidas;
+
+namespace MonopolyPaperMario.Components.Stores;
+
+internal class ControlePartidaStore
+{
+    public static event Action? OnStateChanged;
+
+    private static IControlePartida? instance;
+
+    public static IControlePartida ControlePartida
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = new ControlePartida(new Partida(["mario", "papagaio", "princesa"]));
+            }
+            return instance;
+        }
+    }
+
+    public static void NotifyStateChanged()
+    {
+        OnStateChanged?.Invoke();
+    }
+}
